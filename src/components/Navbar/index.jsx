280e7493da6a692f9f77fefdfd90549d5eabec5a
Carlos && Ponce <srtager555@gmail.com>
2021-10-10 22:55:18 -0600

    logic of the menu button ready

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,26 +1,39 @@
-import React, {Fragment} from "react";
-import './css/styles.css'
+import React, { Fragment, useState } from "react";
+import "./css/styles.css";
 
 export default function Navbar() {
+  const [menuInformation, setMenuInformation] = useState('')
+  function handleActiveMenu (){
+    if(menuInformation === ''){
+      setMenuInformation('active')
+    } else {
+      setMenuInformation('')
+    }
+  } 
   return (
     <Fragment>
       <div className="container-menu_options">
         <div className="bx-logo">
           <h1>&&</h1>
         </div>
-        <div className="menu-button">
+        <div className={`menu-button ${menuInformation}`} onClick={handleActiveMenu}>
           <span className="menuLine menuLine-1"></span>
           <span className="menuLine menuLine-2"></span>
           <span className="menuLine menuLine-3"></span>
         </div>
       </div>
-      <div className="container-menu_information">
-        <span>contact</span>
-        <span>linkedin</span>
-        <span>github</span>
-        <span>credits</span>
+      <div className={`container-menu_information ${menuInformation}`}>
+        <div className="background-blur">
+          <div className="filter" />
+        </div>
+        <div className="menu-links">
+          <span>contact</span>
+          <span>linkedin</span>
+          <span>github</span>
+          <span>credits</span>
+        </div>
         <div className="container-form">
-            <form></form>
+          <form></form>
         </div>
       </div>
     </Fragment>
