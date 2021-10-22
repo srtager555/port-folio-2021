81e31d8e1574f41b70f7ded49a0f41841712c17c
Carlos && Ponce <srtager555@gmail.com>
2021-10-22 15:11:49 -0600

    install anime js and apply animation to title of home

--- src/components/Background-header/index.jsx
+++ src/components/Background-header/index.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./css/styles.css";
 
 import VideoPc from "./assets/HuTaoPC.mp4";
+import anime from "animejs";
 // import VideoMb from "./assets/HuTaoMb.mp4";
 
 export default function BackgroundHeader({ children, homeState }) {
@@ -9,9 +10,20 @@ export default function BackgroundHeader({ children, homeState }) {
   let BD = "https://i.ibb.co/0J2Ntfn/hu-Tao-background-desktop.jpg";
   // let BM = "https://i.ibb.co/xSQbqVd/hu-Tao-background-mobil.jpg";
 
+  const elemento = useRef(null)
+  // useEffect(()=>{
+  //   elemento.current = anime({
+  //     targets: ".video-container",
+  //     scale: [10, 1],
+  //     translateY: [0, 100, 0], 
+  //     easing: "easeInOutSine",
+  //     duration: 1000,
+  //   })
+  // }, [])
+
   return (
-    <div className={`container-bg ${homeState? '' : 'off'}`}>
-      <div className="video-container">
+    <div ref={elemento} className={`container-bg ${homeState? '' : 'off'}`}>
+      <div  className="video-container">
           <video poster={BD} src={VideoPc} autoPlay muted loop></video>
       </div>
       <div className="container-title">{children}</div>
