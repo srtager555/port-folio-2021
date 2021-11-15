5022e9a52b2c0f167cf66ae19f43c4935c8b1885
Carlos && Ponce <srtager555@gmail.com>
2021-11-14 21:15:05 -0600

    provisional place holder image created

--- src/components/PlaceHolderImage/index.jsx
+++ src/components/PlaceHolderImage/index.jsx
@@ -6,34 +6,42 @@ export function PlaceHolderImage({ src, alt }) {
   const [loading, setLoading] = React.useState(true);
   const [image, setImage] = React.useState(null);
 
+  const css = {
+    width: "100%",
+    height: "100%",
+    opacity: loading ? 0 : 1,
+    transition: "opacity 0.5s ease-in-out",
+  };
+
   return (
     <Fragment>
       <div className="image-container">
         <img
           src={src}
           alt={alt}
-          onLoad={() => setTimeout(() => setLoading(false), 1000)}
+          onLoad={() => setTimeout(() => setLoading(false), 5000)}
           onError={() => setImage(null)}
-          style={{
-            width: "100%",
-            height: "100%",
-            opacity: loading ? 0 : 1,
-            transition: "opacity 0.5s ease-in-out",
-          }}
+          style={css}
         />
         <div
           className="placeHolder-image"
           style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
             position: "absolute",
             top: 0,
             left: 0,
             width: "100%",
             height: "100%",
-            backgroundColor: loading ? "rgba(0,0,0, 0.1)" : "transparent",
+            backgroundColor: "rgba(0,0,0, 0.1)",
+            opacity: loading ? "1" : "0",
             transition: "background-color 0.5s ease-in-out",
             zIndex: 1,
           }}
-        ></div>
+        >
+          Loading...
+        </div>
       </div>
     </Fragment>
   );
